384ad24d63b6f7e1127ddbf95d9a8313e7f92d15
Arpit Semwal <arpitsemwal@Arpits-MacBook-Air.local>
2022-06-19 14:48:37 -0700

    Moved main component to components directory

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MainPageComponent from './MainPageComponent';
+import MainPageComponent from './components/MainPageComponent';
 
 export default function App() {
   return (
