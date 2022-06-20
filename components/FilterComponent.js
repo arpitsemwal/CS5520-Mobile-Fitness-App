538696fe4e745e9ce7fe860dc5d582ae4e8f2881
Arpit Semwal <arpitsemwal@Arpits-MacBook-Air.local>
2022-06-19 19:50:29 -0700

    Added styling on filter component

--- components/FilterComponent.js
+++ components/FilterComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, Switch, StyleSheet } from 'react-native'
+import { View, Text, Switch, StyleSheet, ImageBackground } from 'react-native'
+
+const image = { uri: "https://mfiles.alphacoders.com/683/thumb-1920-683110.jpg" };
 
 export default class ChangeImageComponent extends Component {
 
@@ -35,23 +37,16 @@ export default class ChangeImageComponent extends Component {
     }
 
     render() {
-        console.log("abs" + this.state.abs)
-        console.log("arms" + this.state.arms)
-        console.log("chest" + this.state.chest)
-        console.log("back" + this.state.back)
-        console.log("shoulder" + this.state.shoulder)
-        console.log("legs" + this.state.legs)
-        console.log("wristInjury" + this.state.wristInjury)
-        console.log("elbowInjury" + this.state.elbowInjury)
-        console.log("shoulderInjury" + this.state.shoulderInjury)
-        console.log("lowerBackInjury" + this.state.lowerBackInjury)
-        console.log("kneeInjury" + this.state.kneeInjury)
         return (
             <View style={styles.container}>
-                <Text> Muscle groups: </Text>
+            <ImageBackground source={image} style={styles.image}>
+                <Text style={styles.title}> Muscle groups: </Text>
                 <View style={styles.switchButton}>
-                    <Text> Abs </Text>
+                    <Text style={styles.filterText}> Abs </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.abs}
                         onValueChange = {(value) => this.setState(
                             {abs: value}
@@ -59,8 +54,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Arms </Text>
+                    <Text style={styles.filterText}> Arms </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.arms}
                         onValueChange = {(value) => this.setState(
                             {arms: value}
@@ -68,8 +66,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Chest </Text>
+                    <Text style={styles.filterText}> Chest </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.chest}
                         onValueChange = {(value) => this.setState(
                             {chest: value}
@@ -77,8 +78,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Back </Text>
+                    <Text style={styles.filterText}> Back </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.back}
                         onValueChange = {(value) => this.setState(
                             {back: value}
@@ -86,8 +90,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Shoulder </Text>
+                    <Text style={styles.filterText}> Shoulder </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.shoulder}
                         onValueChange = {(value) => this.setState(
                             {shoulder: value}
@@ -95,18 +102,24 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Legs </Text>
+                    <Text style={styles.filterText}> Legs </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.legs}
                         onValueChange = {(value) => this.setState(
                             {legs: value}
                         )}
                     />
                 </View>
-                <Text> Injury: </Text>
+                <Text style={styles.title}> Injury: </Text>
                 <View style={styles.switchButton}>
-                    <Text> Wrist </Text>
+                    <Text style={styles.filterText}> Wrist </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.wristInjury}
                         onValueChange = {(value) => this.setState(
                             {wristInjury: value}
@@ -114,8 +127,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Elbow </Text>
+                    <Text style={styles.filterText}> Elbow </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.elbowInjury}
                         onValueChange = {(value) => this.setState(
                             {elbowInjury: value}
@@ -123,8 +139,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Shoulder </Text>
+                    <Text style={styles.filterText}> Shoulder </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.shoulderInjury}
                         onValueChange = {(value) => this.setState(
                             {shoulderInjury: value}
@@ -132,8 +151,11 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Lower Back </Text>
+                    <Text style={styles.filterText}> Lower Back </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.lowerBackInjury}
                         onValueChange = {(value) => this.setState(
                             {lowerBackInjury: value}
@@ -141,14 +163,18 @@ export default class ChangeImageComponent extends Component {
                     />
                 </View>
                 <View style={styles.switchButton}>
-                    <Text> Knee </Text>
+                    <Text style={styles.filterText}> Knee </Text>
                     <Switch
+                        trackColor={{ false: "#999999", true: "#40FF00" }}
+                        ios_backgroundColor = "#999999"
+                        style={styles.switch}
                         value = {this.state.kneeInjury}
                         onValueChange = {(value) => this.setState(
                             {kneeInjury: value}
                         )}
                     />
                 </View>
+            </ImageBackground>
             </View>
         )
     }
@@ -161,8 +187,36 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
     switchButton: {
         flexDirection: 'row',
-      },
+        padding: 10,
+    },
+    title: {
+        fontSize: 25,
+        color: "#000",
+        marginRight: 100,
+        marginLeft: 100,
+        color: "white",
+        lineHeight: 84,
+        fontWeight: "bold",
+        backgroundColor: "#000000c0",
+        textAlign: "center",
+    },
+    filterText: {
+        fontSize: 20,
+        color: "#000",
+        color: "white",
+        textAlign: "right",
+        marginRight: 80,
+        marginLeft: 80,
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        resizeMode: "cover"
+    },
+    switch: {
+        marginRight: 10,
+    }
 });
