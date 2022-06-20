2fe3cbeb6ce8f59742494f1e0cef16af3e33f4d7
ratnakirti <email.navneetr@gmail.com>
2022-06-20 00:04:03 -0700

    Implemented TimerComponent

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainPageComponent from './components/MainPageComponent';
+import TimerComponent from './components/TimerComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MainPageComponent/>
+      {/* <MainPageComponent/> */}
+      <TimerComponent/>
       <StatusBar style="auto" />
       
     </View>
